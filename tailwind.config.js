8cdde71a460f3063a041ba6af5a8fcc614898c76
Mohaned <mohanedmohamed267@gmail.com>
2024-11-27 16:54:51 +0200

    add registration page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sand: '#E0C097',
+        desert: '#D88A48',
+        sunset: '#C64924',
+        'black-dark':'#00000050',
+        'white-light':'#FFFFFF30',
+        'white-light':'#FFFFFF30',
+        'white-medium':'#FFFFFF40',
+      },
+    },
   },
   plugins: [],
 }

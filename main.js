96c2c72177b857ae9a88460dfc3be48753d39487
Mohaned <mohanedmohamed267@gmail.com>
2024-11-27 18:33:45 +0200

    adding login page and make some mantinance

---
+++ main.js
@@ -0,0 +1,6 @@
+let register_button = document.getElementById('Register_button');
+
+
+register_button.addEventListener('click', function (event) {
+    window.location.href = './Registration.html'; 
+});

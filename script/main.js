dbf409a63539ec05fc8ff5221d89d354f3ded706
Hossam-Eldin-Omar <omarhossam524@gmail.com>
2024-12-07 04:53:59 +0200

    Added_FAQs_Contact_Footer_Sections_To_Home

--- script/main.js
+++ script/main.js
@@ -7,6 +7,25 @@ XamrkIcon.classList.add('hidden');
 bar_xmark_toggle.addEventListener('click', function() {
     menuToggle.classList.toggle('hidden');
     BarIcon.classList.toggle('hidden');
-    XamrkIcon.classList.toggle('hidden');
+    XamrkIcon.classList.toggle('hidden'); 
 });
+
+
+function toggle(cardNumber) {
+
+    var p = document.getElementById(`p_${cardNumber}`);
+    var up_icon = document.getElementById(`up_${cardNumber}`);
+    var down_icon = document.getElementById(`down_${cardNumber}`);
+
+    if(getComputedStyle(p).display === "none") {
+        p.style.display = "block";
+        down_icon.style.display = "none";
+        up_icon.style.display = "block";
+    }
+    else {
+        p.style.display = "none";
+        down_icon.style.display = "block";
+        up_icon.style.display = "none";
+    }
+}
 
